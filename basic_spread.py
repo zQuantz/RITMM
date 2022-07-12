c7f3218ad80e7c18541700c01b0634b58eddbde6
unknown <hu.ferretti@gmail.com>
2022-07-12 15:11:10 -0400

    fixed mistake in vol

--- basic_spread.py
+++ basic_spread.py
@@ -12,15 +12,15 @@ def rogers_satchell_vol(ohlc_data, time_step=1):
     hi = ohlc_data['high'].values
     cl = ohlc_data['close'].values
     argument = np.log(hi/cl) * np.log(hi/op) + np.log(lo/cl) * np.log(lo/op)
-    return np.sqrt(1 / time_step * np.sum(argument))
+    return np.sqrt(1 / op.shape[0] * np.sum(argument))
 
 def garman_klass_vol(ohlc_data, time_step=1):
     op = ohlc_data['open'].values
     lo = ohlc_data['low'].values
     hi = ohlc_data['high'].values
     cl = ohlc_data['close'].values
-    argument = np.log(hi/lo) ** 2 - (2 * np.log(2) - 1)/time_step * np.log(cl/op) ** 2 
-    return np.sqrt(1 / 2 *time_step * np.sum(argument))
+    argument = 1/2 * np.log(hi/lo) ** 2 - (2 * np.log(2) - 1) * np.log(cl/op) ** 2 
+    return np.sqrt(1 / op.shape[0] * np.sum(argument))
 
 def return_vol_estimator(close_data):
     time, price = (data['tick'].to_numpy(), data['close'].to_numpy())
