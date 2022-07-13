821e716b82bfe4fbb7ceac60fa4cb0fa70c65ac2
unknown <hu.ferretti@gmail.com>
2022-07-13 10:05:34 -0400

    added global trend indicator

--- volatility_estimators.py
+++ volatility_estimators.py
@@ -9,6 +9,7 @@ def rogers_satchell_vol(ohlc_data, time_step=1):
     argument = np.log(hi/cl) * np.log(hi/op) + np.log(lo/cl) * np.log(lo/op)
     return np.sqrt(1 / op.shape[0] * np.sum(argument))
 
+
 def garman_klass_vol(ohlc_data, time_step=1):
     op = ohlc_data['open'].values
     lo = ohlc_data['low'].values
@@ -17,8 +18,8 @@ def garman_klass_vol(ohlc_data, time_step=1):
     argument = 1/2 * np.log(hi/lo) ** 2 - (2 * np.log(2) - 1) * np.log(cl/op) ** 2 
     return np.sqrt(1 / op.shape[0] * np.sum(argument))
 
-def close_to_close_vol(close_data):
-    time, price = (data['tick'].to_numpy(), data['close'].to_numpy())
+def return_vol_estimator(close_data):
+    price = pd.Series(close_data).values
     x = np.log(price[1:]) - np.log(price[:-1])
     mu = x.mean()
     vol = np.std(x-mu, ddof=1)
@@ -26,3 +27,15 @@ def close_to_close_vol(close_data):
 
 def c2c_vol(ohlc, time_step=1):
     return np.log(1+ohlc.close.pct_change()).std()
+
+def return_vol_error(close_data, time_step=1):
+    mu, vol = return_vol_estimator(close_data)    
+    mu_error = np.sqrt(vol ** 4 / 2 + vol ** 2 / time_step) / np.sqrt(close_data.shape[0])
+    vol_error = np.sqrt(vol ** 2 / 2) / np.sqrt(close_data.shape[0])
+    return mu_error, vol_error
+
+def z_score_trend_indicator(close_data, time_step=1):
+    mu, vol = return_vol_estimator(close_data)
+    mu_error, vol_error = return_vol_error(close_data, time_step=1)
+    zscore = mu / mu_error
+    return zscore
